f57a1086273ebac133cc5cc6368674b1b0868f71
Ibtihall <ibtihalmaghraoui@gmail.com>
2022-05-29 14:10:13 +0100

    leveling up test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Dashboard from './pages/Dashboard/index';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LevelUP from './pages/LevelUpPage';
+import { PAGES } from 'routes';
 
-function App() {
+const ROUTES = [
+  {
+    path: PAGES.ROOT,
+    element: <Dashboard/>,
+  },
+  {
+    path: `${PAGES.LEVELPAGE}/:exp`,
+    element: <LevelUP/>,
+  }, 
+];
+
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {ROUTES.map((props, idx) => (
+          <Route key={idx} {...props} />
+        ))} 
+      </Routes>
+    </Router>
   );
 }
 
